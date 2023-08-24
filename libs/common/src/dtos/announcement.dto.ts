35a4b692bc2d255e9edda646b0b445f8b2c9b866
Aramik <aramikm@gmail.com>
2023-08-24 14:00:08 -0700

    define api endpoints and contract

---
+++ libs/common/src/dtos/announcement.dto.ts
@@ -0,0 +1,32 @@
+// eslint-disable-next-line max-classes-per-file
+import { NoteActivityDto, ProfileActivityDto } from './activity.dto';
+
+// eslint-disable-next-line no-shadow
+export enum AnnouncementTypeDto {
+  TOMBSTONE = 'tombstone',
+  BROADCAST = 'broadcast',
+  REPLY = 'reply',
+  REACTION = 'reaction',
+  PROFILE = 'profile',
+  UPDATE = 'update',
+}
+
+export class BroadcastDto extends NoteActivityDto {}
+
+export class ReplyDto extends NoteActivityDto {
+  inReplyTo: string;
+}
+
+export class UpdateDto extends NoteActivityDto {
+  targetAnnouncementType: AnnouncementTypeDto;
+}
+
+export class ReactionDto {
+  emoji: string;
+
+  apply: number;
+
+  inReplyTo: string;
+}
+
+export class ProfileDto extends ProfileActivityDto {}
---
+++ libs/common/src/dtos/common.dto.ts
@@ -0,0 +1,15 @@
+// eslint-disable-next-line max-classes-per-file
+import { ApiProperty } from '@nestjs/swagger';
+
+export class AnnouncementResponseDto {
+  referenceId: string;
+}
+
+export class UploadResponseDto {
+  assetIds: Array<string>;
+}
+
+export class FilesUploadDto {
+  @ApiProperty({ type: 'array', items: { type: 'string', format: 'binary' } })
+  files: any[];
+}
