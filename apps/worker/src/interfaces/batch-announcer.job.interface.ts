d85f58ff5eb7c1f15910cf450ec80f06e06d0a47
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-08-30 16:59:25 -0500

    add announcements to payload

--- apps/worker/src/interfaces/batch-announcer.job.interface.ts
+++ apps/worker/src/interfaces/batch-announcer.job.interface.ts
@@ -1,4 +1,7 @@
+import { RequestTypeDto } from "../../../../libs/common/src";
+
 export interface IBatchAnnouncerJobData {
   batchId: string;
   schemaId: number;
+  announcements: RequestTypeDto[];
 }
