eb14e0e90cf8f3a02d01440332f6032fe4e30d78
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-09-06 16:44:03 -0500

    cleanupand fix tests

--- apps/api/src/config/env.config.ts
+++ apps/api/src/config/env.config.ts
@@ -6,7 +6,7 @@ export const configModuleOptions: ConfigModuleOptions = {
   isGlobal: true,
   validationSchema: Joi.object({
     IPFS_ENDPOINT: Joi.string().uri().required(),
-    IPFS_GATEWAY_URL: Joi.string().uri().required(),
+    IPFS_GATEWAY_URL: Joi.string().required(), // This is parse as string as the required format of this not a valid uri, check .env.template
     IPFS_BASIC_AUTH_USER: Joi.string().allow('').default(''),
     IPFS_BASIC_AUTH_SECRET: Joi.string().allow('').default(''),
     REDIS_URL: Joi.string().uri().required(),
